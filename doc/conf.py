50712adaa8e2a59605d8c75a3ce6398655dd3d4d
holger krekel <holger@merlinux.eu>
2011-05-25 21:54:59 +0200

    avoid unneccessary pytest reference for creating docs

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ copyright = u'2010, holger krekel et. al.'
 #
 # The short X.Y version.
 # The full version, including alpha/beta/rc tags.
-import py, pytest
+import py
 assert py.path.local().relto(py.path.local(py.__file__).dirpath().dirpath())
 release = py.__version__
 version = ".".join(release.split(".")[:2])
