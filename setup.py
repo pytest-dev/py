f6ac6b02a881a14a8bd98e093f8fe4d1a1917785
holger krekel <holger@merlinux.eu>
2011-11-24 10:21:51 +0000

    try to fix failures related to subversion 1.7.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def main():
         name='py',
         description='library with cross-python path, ini-parsing, io, code, log facilities',
         long_description = open('README.txt').read(),
-        version='1.4.6.dev1',
+        version='1.4.6.dev3',
         url='http://pylib.org',
         license='MIT license',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
