aaeaef37bb57cdd53704e35a9aa2c957bf0fe47d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-04-08 22:52:15 +0200

    python3 bytes decoding cant actually handle None as error handler

--- py/_builtin.py
+++ py/_builtin.py
@@ -115,7 +115,10 @@ if sys.version_info >= (3, 0):
     _basestring = str
     def _totext(obj, encoding=None, errors=None):
         if isinstance(obj, bytes):
-            obj = obj.decode(encoding, errors)
+            if errors is None:
+                obj = obj.decode(encoding)
+            else:
+                obj = obj.decode(encoding, errors)
         elif not isinstance(obj, str):
             obj = str(obj)
         return obj
