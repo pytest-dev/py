257f7e301199cba1ece98465148931b88036a6c1
Floris Bruynooghe <flub@devork.be>
2013-11-24 22:29:33 +0000

    Provide a unicode-preserving repr method
    This is part of pytest issue 319.

--- py/_io/saferepr.py
+++ py/_io/saferepr.py
@@ -12,6 +12,23 @@ class SafeRepr(reprlib.Repr):
     def repr(self, x):
         return self._callhelper(reprlib.Repr.repr, self, x)
 
+    def repr_unicode(self, x, level):
+        # Strictly speaking wrong on narrow builds
+        def repr(u):
+            if "'" not in u:
+                return py.builtin._totext("'%s'") % u
+            elif '"' not in u:
+                return py.builtin._totext('"%s"') % u
+            else:
+                return py.builtin._totext("'%s'") % u.replace("'", r"\'")
+        s = repr(x[:self.maxstring])
+        if len(s) > self.maxstring:
+            i = max(0, (self.maxstring-3)//2)
+            j = max(0, self.maxstring-3-i)
+            s = repr(x[:i] + x[len(x)-j:])
+            s = s[:i] + '...' + s[len(s)-j:]
+        return s
+
     def repr_instance(self, x, level):
         return self._callhelper(builtin_repr, x)
 
