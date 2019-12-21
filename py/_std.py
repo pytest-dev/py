3f46337df19e98908145efd267d83a3e1b7ba954
John Vandenberg <jayvdb@gmail.com>
2019-12-21 10:59:22 +0700

    _std: Show caller source in deprecation message
    Closes https://github.com/pytest-dev/py/issues/227

--- py/_std.py
+++ py/_std.py
@@ -16,7 +16,8 @@ class Std(object):
 
     def __getattr__(self, name):
         warnings.warn("py.std is deprecated, plase import %s directly" % name,
-                      category=PyStdIsDeprecatedWarning)
+                      category=PyStdIsDeprecatedWarning,
+                      stacklevel=2)
         try:
             m = __import__(name)
         except ImportError:
