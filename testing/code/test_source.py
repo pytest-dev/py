2673bde8756d832d138f3f26c291ae15b0b503ed
Christian Clauss <cclauss@me.com>
2020-06-25 15:14:07 +0200

    SyntaxError is 5 in CPython 3.8

--- testing/code/test_source.py
+++ testing/code/test_source.py
@@ -103,7 +103,8 @@ def test_source_strip_multiline():
 def test_syntaxerror_rerepresentation():
     ex = py.test.raises(SyntaxError, py.code.compile, 'xyz xyz')
     assert ex.value.lineno == 1
-    assert ex.value.offset in (4,7) # XXX pypy/jython versus cpython?
+    # SyntaxError is 5 in CPython 3.8
+    assert ex.value.offset in (4, 5, 7) # XXX pypy/jython versus cpython?
     assert ex.value.text.strip(), 'x x'
 
 def test_isparseable():
