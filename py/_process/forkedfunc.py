3348f6178d63a785b77e7bb17917f4b31d098afe
DWesl <22566757+DWesl@users.noreply.github.com>
2020-08-24 06:05:35 -0400

    Change test to be more concise.
    Co-authored-by: Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>

--- py/_process/forkedfunc.py
+++ py/_process/forkedfunc.py
@@ -107,7 +107,7 @@ class ForkedFunc:
             self.tempdir.remove()
 
     def __del__(self):
-        if not (hasattr(self, "pid") and self.pid is None):  # only clean up in main process
+        if  getattr(self, "pid", None) is None:  # only clean up in main process
             self._removetemp()
 
 
